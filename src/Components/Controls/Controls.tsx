2a76c6a490ea02e11040de43b0b374c6e4d20d19
Neel Patel <321neel@gmail.com>
2022-05-05 00:07:29 -0400

    update

---
+++ src/Components/Controls/Controls.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import './Controls.css'
+
+type Props = {
+    setTimeInSeconds: Function
+};
+
+function Controls(props:Props){
+    const { setTimeInSeconds } = props;
+    const [intervalId, setIntervalId] = useState<number>(0);
+
+    const handlePlayButton = () => {
+        let interval:any = setInterval(() => {
+            setTimeInSeconds((previousState:number) => previousState + 1)
+        }, 1000);
+        setIntervalId(interval)
+    }
+
+    const handleStopButton = () => {
+        clearInterval(intervalId)
+    }
+
+    const handleResetButton = () => {
+        clearInterval(intervalId);
+        setTimeInSeconds(0)
+
+    }
+
+    return(
+        <section className="controls-container">
+            <button onClick={handlePlayButton}>Play</button>
+            <button onClick={handleStopButton} >Stop</button>
+            <button onClick={handleResetButton} >Reset</button>
+        </section>
+    )
+}
+
+export default Controls
